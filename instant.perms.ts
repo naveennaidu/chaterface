f56208ffa92c8ee64765d2e90b8467a1646cb78a
John Paul <johnarpaul@gmail.com>
2025-04-03 10:30:37 +0100

    Update instant.perms.ts

--- instant.perms.ts
+++ instant.perms.ts
@@ -3,30 +3,29 @@
 import type { InstantRules } from "@instantdb/react";
 
 const rules = {
-  /**
-   * Welcome to Instant's permission system!
-   * Right now your rules are empty. To start filling them in, check out the docs:
-   * https://www.instantdb.com/docs/permissions
-   *
-   * Here's an example to give you a feel:
-   * posts: {
-   *   allow: {
-   *     view: "true",
-   *     create: "isOwner",
-   *     update: "isOwner",
-   *     delete: "isOwner",
-   *   },
-   *   bind: ["isOwner", "auth.id != null && auth.id == data.ownerId"],
-   * },
-   */
   conversations: {
     allow: {
       view: "data.sessionId == ruleParams.sessionId",
-      create: "data.sessionId == ruleParams.sessionId",
+      create: "true",
       update: "data.sessionId == ruleParams.sessionId",
       delete: "data.sessionId == ruleParams.sessionId",
     },
   },
+  messages: {
+    allow: {
+      view: "data.ref('conversation.sessionId') == ruleParams.sessionId",
+      create: "true",
+      update: "data.ref('conversation.sessionId') == ruleParams.sessionId",
+      delete: "data.ref('conversation.sessionId') == ruleParams.sessionId",
+    },
+  },
+  $files: {
+    allow: {
+      view: "true",
+      create: "true",
+      delete: "true"
+    }
+  }
 } satisfies InstantRules;
 
 export default rules;
